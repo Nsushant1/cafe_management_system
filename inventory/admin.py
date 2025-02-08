9eae904ff99f4620b69d81176a3d5317054ef9df
Sushant Neupane <nsushaant72@gmail.com>
2025-02-08 14:22:08 +0545

    updated

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
 
+from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import Category, Supplier, InventoryItem
 # Register your models here.
+@admin.register(InventoryItem)
+class InventoryItemAdmin(ImportExportModelAdmin):
+    list_display = ('name', 'category', 'quantity', 'unit',)
+    search_fields = ("name", "category__name")
+
+admin.site.register(Category)
+admin.site.register(Supplier)
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+    
+class Supplier(models.Model):
+    name=models.CharField(max_length=255)
+    contact=models.CharField(max_length=255)
+    email=models.EmailField()
+    def __str__(self):
+        return self.name
+    
+class InventoryItem (models.Model):
+    UNITS = [
+        ('g', 'Grams'),
+        ('kg', 'Kilograms'),
+        ('ml', 'Milliliters'),
+        ('L', 'Liters'),
+        ('unit', 'Unit'),
+    ]
+    name= models.CharField(max_length=255)
+    category=models.ForeignKey(Category,on_delete=models.SET_NULL,null=True ,blank=True)
+    quantity=models.FloatField()
+    unit=models.CharField(max_length=10,choices=UNITS)
+    supplier = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}({self.quantity} {self.unit})"
+
+
